95304b5b2ad5d2d9e298176a4462fcaeef35de81
William Sharp <wsharp@ltu.edu>
2016-11-15 22:09:26 -0500

    adding logging using syslog. applying naming rules according to c++ core guidelines. changing the example so it reads from a queue. simplifying the example to just display card data

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <termios.h>
-#include <time.h>
-#include <ctype.h>
+#include <syslog.h>
 #include <signal.h>
+#include <cstdlib>
+#include <string>
 #include "lib/pcProxAPI/pcProxAPI.h"
 
 using namespace std;
@@ -15,41 +12,28 @@ void inthand(int signum) {
     stop = 1;
 }
 
-//
-// Show returned CSN from Queued ID function call
-//
-void ShowQueuedID(short cid, short chold)
+void start_logging(const char* filename)
 {
-    BYTE buf[36];
-    GetQueuedID(cid,chold);
-    for(short j=0; j<35; j++)
-    {
-        buf[j] = GetQueuedID_index(j);
-    }
-    int nBits = buf[32];
-    int age = buf[33];
-    int ovr = buf[34];
-    int lot = buf[35];
-
-    unsigned int bytes = (nBits+7)/8;
-
-    printf("GetQueuedID: Age %3d, Ovr %3d, Lock %3d, Bits %3d, Data ",age,ovr,lot,nBits);
-    for(unsigned int i=0; i<bytes; i++)
-    {
-        printf("%02X ",buf[i] & 255);
-    }
-    printf("\n");
+    setlogmask (LOG_UPTO (LOG_NOTICE));
+    openlog (filename, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
 }
 
-//
-// Delay N Milliseconds
-//
-void Delay(int ms)
+void stop_logging()
+{
+    closelog();
+}
+
+void log_info(const char* message)
+{
+    syslog(LOG_NOTICE, "%s", message);
+}
+
+void delay(int ms)
 {
     usleep(ms*1000);
 }
 
-int Connect()
+int connect()
 {
     sleep(1);
     USBDisconnect();
@@ -57,11 +41,11 @@ int Connect()
     return usbConnect();
 }
 
-int TryToConnect()
+int try_to_connect()
 {
     int numberOfDevices = 0;
     printf("** Please wait... Trying to connect to reader **\n");
-    bool rc = Connect();
+    bool rc = connect();
     if (rc)
     {
         numberOfDevices = GetDevCnt();
@@ -71,8 +55,45 @@ int TryToConnect()
     return numberOfDevices;
 }
 
-int main() {
+string get_card_id(unsigned int bytes, BYTE buf[36])
+{
+    // 00 00 00 00 00 00
+    char buffer[18];
+    for(unsigned int i=0; i<bytes; i++)
+    {
+        sprintf(&buffer[3*i],"%02X ",buf[i] & 255);
+    }
+
+    return buffer;
+}
+
+void try_get_card_id()
+{
+    BYTE buf[36];
+
+    // Get data from queue and clear
+    GetQueuedID(1,1);
+    for(short j=0; j<35; j++)
+    {
+        buf[j] = GetQueuedID_index(j);
+    }
+    int nBits = buf[32];
+
+    // No data found in queue
+    if (nBits == 0) return;
+
+    // Data found in queue
+    unsigned int bytes = (nBits+7)/8;
+
+    string card_id = get_card_id(bytes,buf);
+    printf("Card Read: %s\n", card_id.c_str());
+}
+
+int main(int argc, char *argv[]) {
+    start_logging("pcprox-console");
     signal(SIGINT, inthand);
+
+    log_info("Starting the console");
 
     printf("\n###\n");
     printf("Starting the console\n");
@@ -85,33 +106,23 @@ int main() {
     {
         if (numberOfDevices == 0)
         {
-            numberOfDevices = TryToConnect();
+            numberOfDevices = try_to_connect();
         }
 
         if(numberOfDevices > 0)
         {
-            unsigned bytes = 0;
-            BYTE buf[40];
-            int nBits = GetActiveID(buf,sizeof(buf));
-            if(nBits > 0)
-            {
-                bytes = (nBits+7)/8;
-                printf("GetActiveCardID: ....................... Bits %3d, Data ",nBits);
-                for(unsigned int i=0; i<bytes; i++)
-                {
-                    printf("%02X ",buf[i] & 255);
-                }
-                printf("\n");
-                ShowQueuedID(0,0);
-            }
-            if(nBits == 0 && GetLastLibErr() > 0)
+            try_get_card_id();
+
+            if(GetLastLibErr() > 0)
             {
                 printf("Lost reader connection\n");
                 numberOfDevices = 0;
             }
-            Delay(250);
+
+            delay(250);
         }
     }
-    return 0;
-}
 
+    stop_logging();
+    return(EXIT_SUCCESS);
+}
