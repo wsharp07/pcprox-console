7906199bd3c107918a26be67d1342c28b4a4a772
William Sharp <wsharp@ltu.edu>
2016-11-16 22:41:47 -0500

    adding a class for logging. cleaning up unnecessary files

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir -p build
 g++ -c -g -Wall -I./lib/pcProxAPI/ -I./lib/hidapi/ main.cpp  -o pcprox-console.o
-g++ -o build/pcprox-console  pcprox-console.o -lpcProxAPI -lhidapi-hidraw -L./lib/pcProxAPI/ -L./lib/hidapi/
+g++ -c -g -Wall syslogger.cpp -o syslogger.o
+g++ -o build/pcprox-console  pcprox-console.o syslogger.o -lpcProxAPI -lhidapi-hidraw -L./lib/pcProxAPI/ -L./lib/hidapi/
 rm pcprox-console.o
+rm syslogger.o
 strip build/pcprox-console
--- main.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by William Sharp on 11/14/16.
-//
-
-#ifndef PCPROX_CONSOLE_MAIN_H
-#define PCPROX_CONSOLE_MAIN_H
-
-#endif //PCPROX_CONSOLE_MAIN_H
