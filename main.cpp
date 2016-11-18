d132de2c34f165e40ee87c68f0cf477be046e0cf
William Sharp <wsharp07@users.noreply.github.com>
2016-11-17 21:22:42 -0500

    Adding a class wrapper around PcProx library (#2)
    * adding the wrapper class for pcprox
    
    * fixing a bug where the card data would not get returned each scan; was missing the delay

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <string>
 #include "lib/pcProxAPI/pcProxAPI.h"
+#include "pcprox.h"
 #include "syslogger.h"
 
 using namespace std;
@@ -17,94 +18,56 @@ void delay(int ms)
     usleep(ms*1000);
 }
 
-int connect()
+void write_greeting()
 {
-    sleep(1);
-    USBDisconnect();
-    SetDevTypeSrch(PRXDEVTYP_USB);
-    return usbConnect();
-}
-
-int try_to_connect()
-{
-    int numberOfDevices = 0;
-    printf("** Please wait... Trying to connect to reader **\n");
-    bool rc = connect();
-    if (rc)
-    {
-        numberOfDevices = GetDevCnt();
-        printf("Number of connected readers: %d\n", numberOfDevices);
-        printf("Ready to accept card scan...\n");
-    }
-    return numberOfDevices;
-}
-
-string get_card_id(unsigned int bytes, BYTE buf[36])
-{
-    // 00 00 00 00 00 00
-    char buffer[18];
-    for(unsigned int i=0; i<bytes; i++)
-    {
-        sprintf(&buffer[3*i],"%02X ",buf[i] & 255);
-    }
-
-    return buffer;
-}
-
-void try_get_card_id()
-{
-    BYTE buf[36];
-
-    // Get data from queue and clear
-    GetQueuedID(1,1);
-    for(short j=0; j<35; j++)
-    {
-        buf[j] = GetQueuedID_index(j);
-    }
-    int nBits = buf[32];
-
-    // No data found in queue
-    if (nBits == 0) return;
-
-    // Data found in queue
-    unsigned int bytes = (nBits+7)/8;
-
-    string card_id = get_card_id(bytes,buf);
-    printf("Card Read: %s\n", card_id.c_str());
-}
-
-int main(int argc, char *argv[]) {
-    Syslogger logger;
-    logger.start_logging("pcprox-console");
-    signal(SIGINT, inthand);
-
-    logger.info("Starting the console with class");
-
     printf("\n###\n");
     printf("Starting the console\n");
     printf("Press CTRL+C to exit\n");
     printf("###\n\n");
+}
 
-    int numberOfDevices = 0;
+int main(int argc, char *argv[]) {
+    // variables
+    Syslogger logger;
+    Pcprox reader;
+    bool is_connected = false;
+
+    signal(SIGINT, inthand);
+
+    logger.start_logging("pcprox-console");
+    logger.info("Starting the console with class");
+
+    write_greeting();
 
     while (!stop)
     {
-        if (numberOfDevices == 0)
+        if (is_connected)
         {
-            numberOfDevices = try_to_connect();
-        }
+            string card_id = reader.get_card_id();
 
-        if(numberOfDevices > 0)
-        {
-            try_get_card_id();
-
-            if(GetLastLibErr() > 0)
+            // There is card data
+            if (card_id.empty() == false)
             {
-                printf("Lost reader connection\n");
-                numberOfDevices = 0;
+                printf("Card Read: %s\n", card_id.c_str());
+
+                if(reader.get_last_lib_err() > 0)
+                {
+                    printf("Lost reader connection\n");
+                    is_connected = false;
+                }
             }
 
             delay(250);
+        }
+        else
+        {
+            printf("** Please wait... Trying to connect to reader **\n");
+            is_connected = reader.connect();
+
+            if (is_connected)
+            {
+                printf("Ready to accept card scan...\n");
+            }
         }
     }
 
